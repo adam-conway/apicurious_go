9c8aefa8891c07838d61efb35d0f44a4015d09cd
Joe Jobes <jmrjobes@gmail.com>
2018-06-13 12:17:29 -0600

    Add GetMeal func in meal_handler
    Add GetMeal func in meal_handler
    Import gorilla/mux for grabbing path variable (params["id"])
    
    Issue #4

--- handler/meal_handler.go
+++ handler/meal_handler.go
@@ -3,6 +3,7 @@ package handler
 import (
   "net/http"
 
+  "github.com/gorilla/mux"
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/postgres"
   "github.com/atmavichara/apicurious_go/models"
@@ -11,5 +12,16 @@ import (
 func GetAllMeals(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
   meals := []models.Meal{}
   db.Find(&meals)
-  respondJSON(w, http.StatusOK, r, meals)
+  RespondJSON(w, http.StatusOK, meals)
+}
+
+func GetMeal(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+  meal := models.Meal{}
+  params := mux.Vars(r)
+
+  if err := db.First(&meal, params["id"]).Error; err != nil {
+    RespondError(w, http.StatusNotFound, err.Error())
+  } else {
+    RespondJSON(w, http.StatusOK, meal)
+  }
 }
