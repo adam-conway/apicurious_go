52d899c04ed798642529e4d48e5cfcd58d35a0d5
Joe Jobes <jmrjobes@gmail.com>
2018-06-13 12:10:55 -0600

    Add show routes
    Add GET /api/v1/meals/:id/foods Endpoint
    Add GET /api/v1/foods/:id Endpoint
    
    Issues #3 #4

--- main.go
+++ main.go
@@ -17,6 +17,21 @@ type App struct {
   DB *gorm.DB
 }
 
+func main() {
+  config := config.GetConfig()
+  a := &App{}
+  a.Init(config)
+  fmt.Println("Server Running on :3000")
+
+  router := mux.NewRouter()
+  sub := router.PathPrefix("/api/v1").Subrouter()
+  sub.HandleFunc("/foods", a.GetAllFoods).Methods("GET")
+  sub.HandleFunc("/meals", a.GetAllMeals).Methods("GET")
+  sub.HandleFunc("/meals/{id}/foods", a.GetMeal).Methods("GET")
+  sub.HandleFunc("/foods/{id}", a.GetFood).Methods("GET")
+  log.Fatal(http.ListenAndServe(":3000", sub))
+}
+
 func (a *App) Init(config *config.Config) {
   dbParams := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
     config.DB.Host,
@@ -29,11 +44,12 @@ func (a *App) Init(config *config.Config) {
   var err error
   a.DB, err = gorm.Open("postgres", dbParams)
   if err != nil {
+    fmt.Println("ERROR IN CONNECTING TO DATABASE\n")
     log.Fatal(err.Error())
   }
 
   fmt.Println("Connected to Database")
-  
+
   a.DB.AutoMigrate(&models.Food{})
   a.DB.AutoMigrate(&models.Meal{})
 }
@@ -46,15 +62,10 @@ func (a *App) GetAllMeals(w http.ResponseWriter, r *http.Request) {
   handler.GetAllMeals(a.DB, w, r)
 }
 
-func main() {
-  config := config.GetConfig()
-  a := &App{}
-  a.Init(config)
-  fmt.Println("Server Running on :3000")
+func (a *App) GetMeal(w http.ResponseWriter, r *http.Request) {
+  handler.GetMeal(a.DB, w, r)
+}
 
-  router := mux.NewRouter()
-  sub := router.PathPrefix("/api/v1").Subrouter()
-  sub.HandleFunc("/foods", a.GetAllFoods).Methods("GET")
-  sub.HandleFunc("/meals", a.GetAllMeals).Methods("GET")
-  log.Fatal(http.ListenAndServe(":3000", sub))
+func (a *App) GetFood(w http.ResponseWriter, r *http.Request) {
+  handler.GetFood(a.DB, w, r)
 }
